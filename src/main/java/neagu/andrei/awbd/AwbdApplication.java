a38a7195b6247e6b678967c103a18241bf704a90
andreineaguc <andreineagu.c@gmail.com>
2020-02-25 20:42:03 +0200

    first-commit

---
+++ src/main/java/neagu/andrei/awbd/AwbdApplication.java
@@ -0,0 +1,13 @@
+package neagu.andrei.awbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neagu/andrei/awbd/AwbdApplicationTests.java
@@ -0,0 +1,13 @@
+package neagu.andrei.awbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
